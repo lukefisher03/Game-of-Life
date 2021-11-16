d7d49bdb243b83e116adbc01c05fa046da45f304
luketheduke112 <lukedavidfisher03@gmail.com>
2021-11-15 22:12:28 -0500

    Now you can click to generate new cells and kill living cells

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let main_canvas = document.getElementById("main-canvas")
 let ctx = main_canvas.getContext("2d")
 
 // [rows, columns]
-size = [50,50]//You will be able to zoom in our out using the size feature in a later implementation.
-ts = 10//Tile size
+size = [10,10]//You will be able to zoom in our out using the size feature in a later implementation.
+ts = 20//Tile size
 board = []
 
 class Cell {
@@ -50,16 +50,31 @@ console.log(board)
 
 test_cell = new Cell(100,100)
 createGrid()
-let x = 1
+main_canvas.addEventListener("click", (event) => {
+    let coors = [event.clientX, event.clientY]
+    for(i = 0; i < board.length; i++) {
+        for(let i2 = 0; i2 < board[0].length; i2++) {
+            if(coors[0] > board[i][i2].x && coors[0] < board[i][i2].x + ts && coors[1] > board[i][i2].y && coors[1] < board[i][i2].y + ts) {
+                if(board[i][i2].life == 0) {   
+                    board[i][i2].life = 1
+                }else if(board[i][i2].life == 1) {
+                    board[i][i2].life = 0
+                }
+            }
+        }
+    }
+    
+
+})
 function draw() {
     ctx.clearRect(0, 0, 1000, 1000); // clear canvas
-    board[x][0].life = 1
-    board[x-1][0].life = 0
-    if(x <= board.length-2){
-        x++
-    }
+    
+
 
     drawGrid()
     window.requestAnimationFrame(draw)
 }
+console.log(board[4][5].x)
+
+
 window.requestAnimationFrame(draw)
